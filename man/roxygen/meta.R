99976f6458a97de191f18f15effc732508963a65
Salim B <git@salim.space>
2023-09-22 02:16:55 +0200

    cosmetic change

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,5 +1,5 @@
 list(
   rd_family_title = list(dev_env = "Other development environment currentness functions:",
-                         pandoc  = "Other Pandoc-related functions:",
+                         pandoc  = "Other Pandoc functions:",
                          spoken  = "Other spoken language functions:")
 )
